356ae93609aca5141ed1ac8c81677d80471ade3a
Peter Grimm <pagrimm@gmail.com>
2020-07-03 10:20:43 -0700

    add function to populate select inputs based on glossary contents

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import 'bootstrap';
 import './bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+//import {ExchangeService} from './exchange-service.js'
+import { ExchangeGlossary } from './../src/currency-exchanger.js';
 
 $(document).ready(function() {
+  let glossary = new ExchangeGlossary();
+  populateSelect(glossary);
   $("form").submit(function(event) {
     event.preventDefault();
   });
 });
 
+function populateSelect (glossary) {
+  for (const currency in glossary) {
+    $("#from-input").append(`<option value="${currency}">${glossary[currency]}</option>`);
+    $("#to-input").append(`<option value="${currency}">${glossary[currency]}</option>`);
+  }
+}
