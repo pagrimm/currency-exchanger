52cd7c80269030df373eb93db13d83f09751dbca
Peter Grimm <pagrimm@gmail.com>
2020-07-03 10:57:35 -0700

    add function to show results

--- src/main.js
+++ src/main.js
@@ -15,7 +15,19 @@ $(document).ready(function() {
 
 function populateSelect (glossary) {
   for (const currency in glossary) {
-    $("#from-input").append(`<option value="${currency}">${glossary[currency]}</option>`);
-    $("#to-input").append(`<option value="${currency}">${glossary[currency]}</option>`);
+    $("#from-input").append(`<option value="${currency}">${glossary[currency].name}</option>`);
+    $("#to-input").append(`<option value="${currency}">${glossary[currency].name}</option>`);
+  }
+}
+
+function showResults (amount, from, to, glossary, response) {
+  if (to === "All") {
+    for (const currency in response.conversion_rates) {
+      let convertedAmount = (amount * response.conversion_rates[currency]).toFixed(2);
+      $("#output-area").append(`<div>${amount} in ${glossary[from].name} is equal to ${convertedAmount} in ${glossary[currency].name}.</div>`)
+    } else {
+      let convertedAmount = (amount * response.conversion_rates[to]).toFixed(2);
+      $("#output-area").append(`<div>${amount} in ${glossary[from].name} is equal to ${convertedAmount} in ${glossary[to].name}.</div>`)
+    }
   }
 }
